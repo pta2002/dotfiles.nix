a3dd15c45ff0dc2823ae49bd33e0acacf17f0c2a
Pedro Alves <pta2002@pta2002.com>
2021-03-18 23:54:44 +0000

    Use nixvim

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,63 +1,101 @@
 { pkgs, config, ... }:
+let
+  nixvim = import (builtins.fetchGit {
+    url = "https://github.com/pta2002/nixvim";
+    ref = "main";
+  });
+in
 {
   imports = [
     ../colorscheme.nix
     ./lsp
     ./plugins/completion-nvim.nix
+    nixvim.homeManagerModules.nixvim
   ];
 
-  programs.neovim = {
-    enable  = true;
+  programs.nixvim = {
+    enable = true;
     package = pkgs.neovim-nightly;
 
-    lsp.clangd.enable = true;
-    lsp.rust-analyzer.enable = true;
-    lsp.rnix-lsp.enable = false;
+    colorschemes.onedark.enable = true;
 
-    lsp.onAttach = builtins.readFile ./lsp_attach.lua;
+    plugins = {
+      lsp = {
+        enable = true;
+        servers.clangd.enable = true;
+      };
 
-    viAlias  = true;
-    vimAlias = true;
+      treesitter = {
+        enable = true;
+        disabledLanguages = [ "ledger" ];
+      };
 
-    extraConfig = builtins.readFile ./init.vim;
+      airline = {
+        enable = true;
+        powerline = true;
+      };
 
-    completion-nvim.enable = true;
+      startify = {
+        enable = true;
+        useUnicode = true;
+      };
 
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
+      telescope = {
+        enable = true;
+        extensions.frecency.enable = true;
+      };
+
+      lspsaga.enable = true;
+      commentary.enable = true;
+      ledger.enable = true;
+      nvim-autopairs.enable = true;
+      undotree.enable = true;
+      gitgutter.enable = true;
+      fugitive.enable = true;
+
+    };
+    extraPlugins = with pkgs.vimPlugins; [
       vim-toml
-
-      vim-commentary
       vim-surround
       editorconfig-vim
-
-      {
-        plugin = nvim-treesitter;
-        config = ''
-          lua <<EOF
-          require'nvim-treesitter.configs'.setup {
-            ensure_installed = "maintained",
-            highlight = {
-              enable = true,
-              disable = { "ledger" }
-            },
-          }
-          EOF
-        '';
-      }
-
-      config.user.colorscheme.vimPlugin
-
-      lightline-vim
       vim-table-mode
-
       tabular
       vim-markdown
-      vim-ledger
 
       vim-terraform
       vim-elixir
       auto-pairs
     ];
+
+    options = {
+      mouse = "a";
+      number = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      expandtab = 2;
+      smarttab = true;
+      autoindent = true;
+      cindent = true;
+      linebreak = true;
+      hidden = true;
+
+      nobackup = true;
+      cot = [ "menuone" "preview" ];
+
+      wildmenu = true;
+      ignorecase = true;
+    };
+
+    globals = {
+      vimsyn_embed = "l";
+      mapleader = " ";
+    };
+
+    maps = {
+      normalVisualOp."ç" = ":";
+      normalVisualOp."-" = "/";
+      normal."j" = "gj";
+      normal."k" = "gk";
+    };
   };
 }
