2b7f0c29b5bea5814adb640dc62f23ca8b65ccbf
Pedro Alves <pta2002@pta2002.com>
2021-03-31 14:31:03 +0100

    git: setup rebase

--- modules/git.nix
+++ modules/git.nix
@@ -3,5 +3,9 @@
     enable = true;
     userName = "Pedro Alves";
     userEmail = "pta2002@pta2002.com";
+    extraConfig = {
+      pull.rebase = true;
+      init.defaultBranchName = "main";
+    };
   };
 }
